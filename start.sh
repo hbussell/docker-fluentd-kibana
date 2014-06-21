59c595150bc25cdc55bff399516673796fd4a1f9
Harley Bussell <modmac@gmail.com>
2014-06-21 11:28:30 +1000

    Update name

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo docker run -p 9200:9200 -p 9300:9300 -p 24224:24224 -p 80:80 -v /data:/data  -name monitoring_instance6 -i -t monitoring
+sudo docker run -p 9200:9200 -p 9300:9300 -p 24224:24224 -p 80:80 -v /data:/data  -name docker_fluent_kibana_inst -i -t docker_fluent_kibana
 
